d313a5ecbf5cf3d9a2e2fe0e35aa11656a18b6d8
zhenqiiii <zhangwenyang2004@foxmail.com>
2025-08-03 21:46:00 +0800

    ✨ feat(Message&frontend): 实现多类型消息发送
    添加消息体Type类型字段，同时prompt出多类型消息发送的前端页面

--- models/message_basic.go
+++ models/message_basic.go
@@ -2,13 +2,25 @@ package models
 
 import "time"
 
+type MessageType string
+
+const (
+	MsgTypeText   MessageType = "text"
+	MsgTypeEmoji  MessageType = "emoji"
+	MsgTypeImage  MessageType = "image"
+	MsgTypeFile   MessageType = "file"
+	MsgTypeVideo  MessageType = "video"
+	MsgTypeSpeech MessageType = "speech"
+)
+
 // 消息结构体
 type MessageBasic struct {
-	UserID    string    `json:"user_id" gorm:""`
-	RoomID    string    `json:"room_id"`
-	Data      string    `json:"data"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	UserID    string      `json:"user_id" gorm:""`
+	RoomID    string      `json:"room_id"`
+	Data      string      `json:"data"`
+	Type      MessageType `json:"type"`
+	CreatedAt time.Time   `json:"created_at"`
+	UpdatedAt time.Time   `json:"updated_at"`
 }
 
 // 指定数据库表名
